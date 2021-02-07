d2c60be289fe6f6278b8b182f838522d050e1e9c
官萍 <ping.guan@cyber-insight.com>
2021-02-07 10:29:39 +0800

    eject

--- template/react-mfe-sub/lib/scripts/start.js
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-const path = require("path");
-const { isReactBuild } = require("./utils");
-
-module.exports = function() {
-  const isReact = isReactBuild();
-  return isReact ? require(`./react/webpack.dev`) : require('./vue/webpack.dev')
-}
